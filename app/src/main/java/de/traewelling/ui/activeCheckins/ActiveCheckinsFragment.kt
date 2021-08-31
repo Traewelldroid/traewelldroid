c9091cefe42de48825c6b64ebff8d93a434abb4b
Jonas Heubuch <jheubuch@christ-es.de>
2021-08-31 15:58:34 +0200

    added MapView, Dashboard changes, Login screen changes

--- app/src/main/java/de/traewelling/ui/activeCheckins/ActiveCheckinsFragment.kt
+++ app/src/main/java/de/traewelling/ui/activeCheckins/ActiveCheckinsFragment.kt
@@ -1,11 +1,20 @@
 package de.traewelling.ui.activeCheckins
 
+import android.os.Build
 import android.os.Bundle
+import android.security.NetworkSecurityPolicy
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import de.traewelling.databinding.FragmentActiveCheckinsBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import java.util.jar.Manifest
 
 class ActiveCheckinsFragment : Fragment() {
 
@@ -16,7 +25,26 @@ class ActiveCheckinsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Configuration.getInstance().userAgentValue = requireContext().packageName
+
         binding = FragmentActiveCheckinsBinding.inflate(inflater, container, false)
+
+        binding.activeCheckinsMap.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        binding.activeCheckinsMap.setMultiTouchControls(true)
+        val controller = binding.activeCheckinsMap.controller
+        controller.setZoom(12.0)
+        controller.setCenter(GeoPoint(47.98, 10.18))
+
         return binding.root
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.activeCheckinsMap.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.activeCheckinsMap.onResume()
     }
 }
