c192430ec9984512e554f8c7d42bc031604e56a6
Jonas Heubuch <jonas.heubuch@outlook.com>
2021-08-31 20:58:38 +0200

    added check-in overview card layout

--- app/src/main/java/de/traewelling/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/de/traewelling/ui/dashboard/DashboardFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import de.traewelling.databinding.FragmentDashboardBinding
+import de.traewelling.models.CheckIn
 
 class DashboardFragment : Fragment() {
 
@@ -17,6 +18,25 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        binding.testCheckIn.checkIn = CheckIn(
+            "Memmingen",
+            "München Hbf",
+            "RE 72",
+            "120 km",
+            "1h 35min",
+            "16:08",
+            "Buchloe",
+            "17:43",
+            "gertrud",
+            "16:04",
+            true
+        )
+
         return binding.root
     }
 }
