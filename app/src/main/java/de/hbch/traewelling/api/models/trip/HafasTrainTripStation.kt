a2efa86a153eb5124ff34af75ed5145148bcac78
Michael Rittmeister <michael@rittmeister.in>
2022-10-09 14:12:39 +0200

    Fix issue with last stop of premature ending train being marked as cancelled

--- app/src/main/java/de/hbch/traewelling/api/models/trip/HafasTrainTripStation.kt
+++ app/src/main/java/de/hbch/traewelling/api/models/trip/HafasTrainTripStation.kt
@@ -9,16 +9,20 @@ data class HafasTrainTripStation(
     @SerializedName("rilIdentifier") val ds100: String,
     @SerializedName("arrival") val arrival: Date,
     @SerializedName("arrivalPlanned") val arrivalPlanned: Date,
-    @SerializedName("arrivalReal") val arrivalReal: Date,
+    @SerializedName("arrivalReal") val arrivalReal: Date?,
     @SerializedName("arrivalPlatformPlanned") val arrivalPlatformPlanned: String,
     @SerializedName("arrivalPlatformReal") val arrivalPlatformReal: String,
     @SerializedName("departure") val departure: Date,
     @SerializedName("departurePlanned") val departurePlanned: Date,
-    @SerializedName("departureReal") val departureReal: Date,
+    @SerializedName("departureReal") val departureReal: Date?,
     @SerializedName("departurePlatformPlanned") val departurePlatformPlanned: String,
     @SerializedName("departurePlatformReal") val departurePlatformReal: String,
     @SerializedName("platform") val platform: String,
     @SerializedName("isArrivalDelayed") val isArrivalDelayed: Boolean,
     @SerializedName("isDepartureDelayed") val isDepartureDelayed: Boolean,
-    @SerializedName("cancelled") val isCancelled: Boolean
-)
+    val cancelled: Boolean
+) {
+    val isCancelled: Boolean
+        // DB Rest also shows last stop as cancelled
+        get() = cancelled && departureReal == null && arrivalReal == null
+}
