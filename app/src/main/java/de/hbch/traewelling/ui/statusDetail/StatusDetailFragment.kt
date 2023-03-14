eb332a3d1bf5bb3cf4351f7bd14adb874f5bd8fd
Jonas Heubuch <jonas.heubuch@outlook.com>
2023-03-14 16:43:01 +0100

    :busts_in_silhouette: added OSM copyright notice

--- app/src/main/java/de/hbch/traewelling/ui/statusDetail/StatusDetailFragment.kt
+++ app/src/main/java/de/hbch/traewelling/ui/statusDetail/StatusDetailFragment.kt
@@ -12,19 +12,38 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import de.hbch.traewelling.BuildConfig
 import de.hbch.traewelling.R
 import de.hbch.traewelling.databinding.FragmentStatusDetailBinding
 import de.hbch.traewelling.shared.CheckInViewModel
 import de.hbch.traewelling.shared.LoggedInUserViewModel
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.MapTileProviderBasic
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.tileprovider.tilesource.TileSourcePolicy
 import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.CopyrightOverlay
 import org.osmdroid.views.overlay.Polyline
 import org.osmdroid.views.overlay.TilesOverlay
 
 class StatusDetailFragment : Fragment() {
+
+    private val MAPNIK: OnlineTileSourceBase = XYTileSource(
+        "Mapnik",
+        0, 19, 256, ".png", arrayOf(
+            "https://a.tile.openstreetmap.org/",
+            "https://b.tile.openstreetmap.org/",
+            "https://c.tile.openstreetmap.org/"
+        ), "© OpenStreetMap contributors, Style: CC-BY-SA 2.0, OpenRailwayMap",
+        TileSourcePolicy(
+            2,
+            TileSourcePolicy.FLAG_NO_BULK
+                    or TileSourcePolicy.FLAG_NO_PREVENTIVE
+                    or TileSourcePolicy.FLAG_USER_AGENT_MEANINGFUL
+                    or TileSourcePolicy.FLAG_USER_AGENT_NORMALIZED
+        )
+    )
 
     private lateinit var binding: FragmentStatusDetailBinding
     private val checkInViewModel: CheckInViewModel by activityViewModels()
@@ -84,8 +103,8 @@ class StatusDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStatusDetailBinding.inflate(inflater, container, false)
-        Configuration.getInstance().userAgentValue = "de.hbch.traewelldroid/1.0"
-        binding.mapStatusDetail.setTileSource(TileSourceFactory.MAPNIK)
+        Configuration.getInstance().userAgentValue = "${BuildConfig.APPLICATION_ID}/${BuildConfig.VERSION_NAME}"
+        binding.mapStatusDetail.setTileSource(MAPNIK)
         binding.mapStatusDetail.setMultiTouchControls(true)
         binding.mapStatusDetail.controller.setZoom(17.5)
 
@@ -94,6 +113,12 @@ class StatusDetailFragment : Fragment() {
 
         val tilesOverlay = TilesOverlay(tileProvider, requireContext())
         binding.mapStatusDetail.overlays.add(tilesOverlay)
+
+        val copyrightOverlay = CopyrightOverlay(requireContext()).apply {
+            setAlignRight(true)
+            setTextSize(8)
+        }
+        binding.mapStatusDetail.overlays.add(copyrightOverlay)
 
         binding.fragment = this
         binding.lifecycleOwner = viewLifecycleOwner
