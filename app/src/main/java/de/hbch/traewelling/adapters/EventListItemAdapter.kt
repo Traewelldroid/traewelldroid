845e42b00c2820caa7084ad83e022db0c1b6d65d
Jonas Heubuch <jonas.heubuch@outlook.com>
2023-05-30 12:10:12 +0200

    :bug: Fix build

--- app/src/main/java/de/hbch/traewelling/adapters/EventListItemAdapter.kt
+++ app/src/main/java/de/hbch/traewelling/adapters/EventListItemAdapter.kt
@@ -1,16 +1,10 @@
 package de.hbch.traewelling.adapters
 
 import android.view.LayoutInflater
-import android.view.View
-import android.view.View.INVISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import de.hbch.traewelling.R
 import de.hbch.traewelling.api.models.event.Event
-import de.hbch.traewelling.api.models.trip.HafasTrainTripStation
 import de.hbch.traewelling.databinding.EventListItemBinding
-import de.hbch.traewelling.databinding.TravelStopListItemBinding
-import de.hbch.traewelling.models.TravelStop
 
 class EventListItemAdapter(val events: List<Event>, val onClick: (Event) -> Unit)
     : RecyclerView.Adapter<EventListItemAdapter.EventViewHolder>() {
