8bf56a62b6674af6b5d6f6641dc9fe2b4188f890
Jonas Heubuch <jonas+github@hbch.de>
2023-07-05 11:17:05 +0200

    :lipstick: Improve text readability on dark theme for older Androids (#164)
    :lipstick: Improve text readability on dark theme for older Android versions

--- app/src/main/java/de/hbch/traewelling/theme/Colors.kt
+++ app/src/main/java/de/hbch/traewelling/theme/Colors.kt
@@ -3,4 +3,5 @@ package de.hbch.traewelling.theme
 import androidx.compose.ui.graphics.Color
 
 val Traewelldroid = Color(0xFF3F51B5)
+val TraewelldroidDark = Color(0xFF42A5F5)
 val StarYellow = Color(0xFFFFC107)
