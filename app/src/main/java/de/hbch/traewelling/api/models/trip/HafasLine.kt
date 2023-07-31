9ed928cb20077cd293610b3a2e23c91f8129b387
Jonas Heubuch <jonas+github@hbch.de>
2023-07-31 15:41:34 +0200

    :lipstick: Add icon for ferries! ⛵ (#204)

--- app/src/main/java/de/hbch/traewelling/api/models/trip/HafasLine.kt
+++ app/src/main/java/de/hbch/traewelling/api/models/trip/HafasLine.kt
@@ -1,6 +1,5 @@
 package de.hbch.traewelling.api.models.trip
 
-import android.content.res.Resources
 import com.google.gson.annotations.SerializedName
 import de.hbch.traewelling.R
 
@@ -13,11 +12,13 @@ data class HafasLine(
     @SerializedName("operator") val operator: HafasOperator?
 )
 
+@Suppress("unused")
 enum class ProductType {
     @SerializedName("all")
     ALL,
     @SerializedName("ferry")
     FERRY {
+        override fun getIcon() = R.drawable.ic_ferry
         override fun getString() = R.string.product_type_ferry
     },
     @SerializedName("bus")
@@ -73,21 +74,4 @@ enum class ProductType {
     open val isTrain = false
     open fun getIcon() = R.drawable.ic_train
     open fun getString() = R.string.product_type_bus
-
-    companion object {
-        fun toString(resources: Resources, productType: ProductType): String {
-            return resources.getString(when (productType) {
-                BUS -> R.string.product_type_bus
-                FERRY -> R.string.product_type_ferry
-                SUBURBAN -> R.string.product_type_suburban
-                SUBWAY -> R.string.product_type_subway
-                TRAM -> R.string.product_type_tram
-                REGIONAL -> R.string.product_type_regional
-                REGIONAL_EXPRESS -> R.string.product_type_regional_express
-                NATIONAL_EXPRESS -> R.string.product_type_national_express
-                NATIONAL -> R.string.product_type_national
-                else -> R.string.product_type_bus
-            })
-        }
-    }
 }
