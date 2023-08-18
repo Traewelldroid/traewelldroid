4dfeae9545810f92f0568aca7424bad8bcceb252
Jonas Heubuch <jonas+github@hbch.de>
2023-08-18 12:23:28 +0200

    :sparkles: Add daily statistics date selection (#236)

--- app/src/main/kotlin/de/hbch/traewelling/ui/statistics/DailyStatistics.kt
+++ app/src/main/kotlin/de/hbch/traewelling/ui/statistics/DailyStatistics.kt
@@ -7,21 +7,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -31,6 +36,7 @@ import de.hbch.traewelling.api.models.status.Status
 import de.hbch.traewelling.shared.LoggedInUserViewModel
 import de.hbch.traewelling.theme.AppTypography
 import de.hbch.traewelling.theme.MainTheme
+import de.hbch.traewelling.ui.composables.ButtonWithIconAndText
 import de.hbch.traewelling.ui.composables.DataLoading
 import de.hbch.traewelling.ui.composables.OutlinedButtonWithIconAndText
 import de.hbch.traewelling.ui.include.status.CheckInCardViewModel
@@ -38,6 +44,7 @@ import de.hbch.traewelling.ui.include.status.getFormattedDistance
 import de.hbch.traewelling.ui.user.getDurationString
 import de.hbch.traewelling.util.checkInList
 import de.hbch.traewelling.util.getLongLocalDateString
+import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -55,7 +62,7 @@ fun DailyStatistics(
     var isLoading by remember { mutableStateOf(false) }
     var statistics by remember { mutableStateOf<DailyStatistics?>(null) }
 
-    var localDate by remember { mutableStateOf(date) }
+    var localDate by rememberSaveable { mutableStateOf(date) }
 
     LaunchedEffect(localDate) {
         if (statsRequested) {
@@ -89,11 +96,8 @@ fun DailyStatistics(
             modifier = modifier,
             statusSelectedAction = statusSelectedAction,
             statusEditAction = statusEditAction,
-            nextDayAction = {
-                localDate = localDate.plusDays(1)
-            },
-            previousDayAction = {
-                localDate = localDate.minusDays(1)
+            dateSelectedAction = { date ->
+                localDate = date
             }
         )
     }
@@ -102,7 +106,7 @@ fun DailyStatistics(
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
 private fun DailyStatisticsView(
     statistics: DailyStatistics,
@@ -111,23 +115,55 @@ private fun DailyStatisticsView(
     modifier: Modifier = Modifier,
     statusSelectedAction: (Int) -> Unit = { },
     statusEditAction: (de.hbch.traewelling.api.dtos.Status) -> Unit = { },
-    nextDayAction: () -> Unit = { },
-    previousDayAction: () -> Unit = { }
+    dateSelectedAction: (LocalDate) -> Unit = { }
 ) {
     val checkInCardViewModel: CheckInCardViewModel = viewModel()
     val checkIns = remember { mutableStateListOf<Status>().also {
         it.addAll(statistics.statuses)
     } }
+
+    var datePickerVisible by remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+    val ms = date.atTime(12, 0).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+    datePickerState.setSelection(ms)
+
+    if (datePickerVisible) {
+        DatePickerDialog(
+            onDismissRequest = { datePickerVisible = false },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        datePickerVisible = false
+                        val millis = datePickerState.selectedDateMillis ?: Instant.now().toEpochMilli()
+                        val zdt = Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault())
+                        dateSelectedAction(
+                            LocalDate.from(zdt.toLocalDate())
+                        )
+                    },
+                    enabled = datePickerState.selectedDateMillis != null
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.ok)
+                    )
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+
     LazyColumn(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
+            ButtonWithIconAndText(
                 text = getLongLocalDateString(date.atStartOfDay(ZoneId.systemDefault())),
-                style = AppTypography.titleMedium,
-                textAlign = TextAlign.Center
+                drawableId = R.drawable.ic_calendar_checked,
+                onClick = {
+                    datePickerVisible = true
+                }
             )
         }
         item {
@@ -138,14 +174,18 @@ private fun DailyStatisticsView(
                 OutlinedButtonWithIconAndText(
                     text = stringResource(id = R.string.previous),
                     drawableId = R.drawable.ic_previous,
-                    onClick = previousDayAction
+                    onClick = {
+                        dateSelectedAction(date.minusDays(1))
+                    }
                 )
                 if (date != LocalDate.now()) {
                     OutlinedButtonWithIconAndText(
                         text = stringResource(id = R.string.next),
                         drawableId = R.drawable.ic_next,
                         drawableOnStart = false,
-                        onClick = nextDayAction
+                        onClick = {
+                            dateSelectedAction(date.plusDays(1))
+                        }
                     )
                 }
             }
