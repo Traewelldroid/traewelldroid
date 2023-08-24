ad0e35d7506ae8a884aa0db032db13e62c2925ea
Jonas Heubuch <jheubuch@christ-es.de>
2023-08-24 15:37:10 +0200

    :lipstick: Make OSS libs a bit better-looking

--- app/src/main/kotlin/de/hbch/traewelling/ui/info/Info.kt
+++ app/src/main/kotlin/de/hbch/traewelling/ui/info/Info.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.ArrowBack
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -71,7 +73,7 @@ fun InfoScreen(
                             )
                         }
                         LibrariesContainer(
-                            modifier = Modifier.fillMaxSize()
+                            modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(16.dp))
                         )
                     }
                 }
