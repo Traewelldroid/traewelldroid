c8f3d2c8c8ec706c1fbe9c8aac9300c8d73c159a
Jonas Heubuch <jonas+github@hbch.de>
2023-11-30 21:18:45 +0100

    :sparkles: Add vehicle no. + passenger rights tag (#309)

--- app/src/main/kotlin/de/hbch/traewelling/api/models/status/Tag.kt
+++ app/src/main/kotlin/de/hbch/traewelling/api/models/status/Tag.kt
@@ -48,6 +48,13 @@ enum class TagType {
         override val key = "trwl:locomotive_class"
         override val example = R.string.tag_locomotive_class_example
     },
+    @SerializedName("trwl:vehicle_number")
+    VEHICLE_NUMBER {
+        override val icon = R.drawable.ic_vehicle_number
+        override val title = R.string.tag_vehicle_number_title
+        override val key = "trwl:vehicle_number"
+        override val example = R.string.tag_vehicle_number_example
+    },
     @SerializedName("trwl:wagon_class")
     WAGON_CLASS {
         override val icon = R.drawable.ic_wagon_class
@@ -61,6 +68,13 @@ enum class TagType {
         override val title = R.string.tag_staff_role_title
         override val key = "trwl:role"
         override val example = R.string.tag_staff_role_example
+    },
+    @SerializedName("trwl:passenger_rights")
+    PASSENGER_RIGHTS {
+        override val icon = R.drawable.ic_passenger_claim
+        override val title = R.string.tag_passenger_rights_title
+        override val key = "trwl:passenger_rights"
+        override val example = R.string.tag_passenger_rights_example
     },
     UNKNOWN {
         override val icon = R.drawable.ic_unknown
